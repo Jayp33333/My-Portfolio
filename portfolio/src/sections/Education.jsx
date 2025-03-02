c6ff928287cc2c25c4202e7bb3400ac816e3f3c6
Jayp33333 <johnpauljamito5@gmail.com>
2025-03-02 21:16:19 +0800

    update

--- portfolio/src/sections/Education.jsx
+++ portfolio/src/sections/Education.jsx
@@ -5,7 +5,7 @@ import { EDUCATION_TIMELINE } from '../utils/data.js'
 const Education = () => {
   return (
     <section id='education' className=' mt-20'>
-      <div className='container mx-auto px-40'>
+      <div className='container mx-auto px-16 lg:px-64'>
         <div className='w-full lg:w-[60vw] mx-auto mb-28'>
           <h4 className='section-title lg:text-4xl '>Education Timeline</h4>
         </div>
